b9d7c3a797be35e6360b05340cbd322378a063a6
Jamrod <myfavoritenoisemaker@gmail.com>
2020-02-26 17:16:50 -0700

    changed header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import '../App.css'
 
-function Header () {
+function Header() {
     return (
         <div className="flex-container header">
-            <h1>Mixer Assister!</h1>
+            {/* <h1>Mixer Assister!</h1> */}
+            {/* <img src="MixerAssister.png" alt="header" /> */}
         </div>
     )
 }
